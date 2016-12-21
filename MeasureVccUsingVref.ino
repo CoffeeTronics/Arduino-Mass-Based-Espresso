700e3473cc91887e9d1b56e3912dabf18dafe38d
Ross Satchell <ross.satchell@gmail.com>
2016-12-20 17:56:34 -0700

    Add files via upload
    Use this code to accurately measure your AVcc voltage in mV. This makes a HUGE difference to the accuracy of the thermistor readings.

---
+++ MeasureVccUsingVref.ino
@@ -0,0 +1,21 @@
+long readVcc() {
+  long result;
+  // Read 1.1V reference against AVcc
+  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Convert
+  while (bit_is_set(ADCSRA,ADSC));
+  result = ADCL;
+  result |= ADCH<<8;
+  result = 1125300L / result; // Back-calculate AVcc in mV
+  return result;
+}
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println( readVcc(), DEC );
+  delay(100);
+}
